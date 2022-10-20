052882d21d56012c3d71939e63e5756cdbe48e9a
BrandonSS2505 <105601441+BrandonSS2505@users.noreply.github.com>
2022-10-20 12:59:42 -0500

    20-10-22

---
+++ practica06/macros.bat
@@ -0,0 +1,3 @@
+compile=mvn compile
+ejecuta=mvn exec:java -Dexec.mainClass=$1
+compila=mvn compila
--- practica06/src/main/java/mx/uv/App.java
+++ practica06/src/main/java/mx/uv/App.java
@@ -1,5 +1,7 @@
 package mx.uv;
 
+import static spark.Spark.*;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,12 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        get("/hello", (req, res) -> "Hello World");
+        get("/eduardo", (req, res) -> "Hello Eduardo");
+        get("/alex", (req, res) -> "Hello Alex");
+        get("/brandon", (req, res) -> "Hello Brandon");
+        get("/", (req, res) -> "<h1>Bienvenidos</h1><img src='https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/02/322868_1100-800x825.jpg'>");
+
+        
     }
 }
