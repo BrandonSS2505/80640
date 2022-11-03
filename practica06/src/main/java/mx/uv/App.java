77b8e7f0697c86575166e26cd66db8e25e213dbf
BrandonSS2505 <105601441+BrandonSS2505@users.noreply.github.com>
2022-11-03 13:01:32 -0600

    commit

--- practica06/src/main/java/mx/uv/App.java
+++ practica06/src/main/java/mx/uv/App.java
@@ -1,6 +1,9 @@
 package mx.uv;
 
 import static spark.Spark.*;
+import com.google.gson.JsonObject;
+
+
 
 /**
  * Hello world!
@@ -17,6 +20,18 @@ public class App
         get("/brandon", (req, res) -> "Hello Brandon");
         get("/", (req, res) -> "<h1>Bienvenidos</h1><img src='https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/02/322868_1100-800x825.jpg'>");
 
+        post("/", (req, res) ->{
+            System.out.println(req.queryParams("email")+" "+ req.queryParams("password"));
+            System.out.println(req.body());
+            res.status(200);
+            JsonObject oRespuesta= new JsonObject();
+            oRespuesta.addProperty("msj","hola");
+            oRespuesta.addProperty("email", req.queryParams("email"));
+            return oRespuesta;
+            
+        });
+
         
     }
+    
 }
