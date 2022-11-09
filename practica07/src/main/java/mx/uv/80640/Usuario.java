d5a146bc4ee2de837a1a0e665610c79865aafa35
BrandonSS2505 <105601441+BrandonSS2505@users.noreply.github.com>
2022-11-09 13:51:06 -0600

    commit

--- practica07/src/main/java/mx/uv/80640/Usuario.java
+++ practica07/src/main/java/mx/uv/80640/Usuario.java
@@ -1,7 +1,31 @@
-package mx.uv.80640;
+//package mx.uv.c80640;
 
 public class Usuario {
     String id;
     String nombre;
-    String 
+    String password;
+    public Usuario(String id, String nombre, String password){
+        this.id = id;
+        this.nombre=nombre;
+        this.password=password;
+    }
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
 }
