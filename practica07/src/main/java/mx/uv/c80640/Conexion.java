c0aeed34704b6b25ed5d24c1459f6bb9f1e08db1
BrandonSS2505 <105601441+BrandonSS2505@users.noreply.github.com>
2022-11-11 00:59:17 -0600

    commit

---
+++ practica07/src/main/java/mx/uv/c80640/Conexion.java
@@ -0,0 +1,9 @@
+// package mx.uv.c80640;
+
+public class Conexion {
+    private static String url = "jdbc:mysql://127.0.0.1:3306/nombreBD";
+    private static String DriverName = "com.mysql.jdbc.Driver";
+    private static String username = "root";
+    private static String password = "";
+    private static Connection connection = null;
+}
