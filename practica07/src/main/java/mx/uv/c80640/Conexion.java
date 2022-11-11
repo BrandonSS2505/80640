cbb72722e4839f2c7937b1eeb8bd9cb38317e6b8
BrandonSS2505 <105601441+BrandonSS2505@users.noreply.github.com>
2022-11-11 01:04:36 -0600

    commit

--- practica07/src/main/java/mx/uv/c80640/Conexion.java
+++ practica07/src/main/java/mx/uv/c80640/Conexion.java
@@ -1,3 +1,6 @@
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 // package mx.uv.c80640;
 
 public class Conexion {
@@ -6,4 +9,17 @@ public class Conexion {
     private static String username = "root";
     private static String password = "";
     private static Connection connection = null;
+
+    public static Connection getConnection(){
+        try {
+            Class.forName(DriverName);
+            connection = DriverManager.getConnection(url, username, password);
+        }catch(SQLException e){
+            System.out.println(e);
+        }catch(ClassNotFoundException e){
+            System.out.println("no encontro el driver");
+        }
+
+        return connection;
+    }
 }
